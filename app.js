2c4b353308260b647a1dd6c2718c5fbb744f1b9e
Heng <lhengi888@gmail.com>
2018-08-03 02:30:52 -0500

    setting up https ports and servers

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 8080;
+//const httpsPort = 443;
 const path = require('path');
 const bodyParser = require('body-parser');
 const router = express.Router();
-
+const fs = require('fs');
 const apiRoute = require('./controllers/requestHandler');
 
+const httpsOptions = {
+    cert : fs.readFileSync('/etc/letsencrypt/live/lhengi.com/fullchain.pem'),
+    key : fs.readFileSync('/etc/letsencrypt/live/lhengi.com/privkey.pem')
+};
 
 
 app.use('/api',apiRoute);
@@ -21,3 +26,5 @@ app.get('/',function(req,res){
 app.listen(port, function(){
     console.log(`Server start on port ${port}`);
 }); 
+
+httpsOptions.creatServer(httpsOptions,app).listen(443);
